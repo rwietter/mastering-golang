74c262dba0c7454a6c7ae04c6ebcb08f0071fa0a
Maurício Witter <mauriciobw17@gmail.com>
2023-05-24 09:06:20 -0300

    add: pointers

--- 6-ponteiros/main.go
+++ 6-ponteiros/main.go
@@ -3,14 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var x *int // declaração de um ponteiro
-	y := 10
-	x = &y                                  // x e y apontam para o mesmo endereço, ao alterar o valor de y, o valor de x também é alterado
-	fmt.Println("Valor de y:", *x)          // 10
-	fmt.Println("Valor do ponteiro x:", *x) // 10
-	y++                                     // incrementando o valor de y
-	fmt.Println("Valor de y:", *x)          // 11
-	fmt.Println("Valor do ponteiro x:", *x) // 11
+	var x int = 10
+	var y = &x                             // obtém o endereço de memória da variável x
+	fmt.Println("Valor de y:", *y)         // desreferenciação de y (obtém o valor = 10)
+	fmt.Println("Valor do ponteiro x:", x) // 10
+	*y++                                   // desreferenciação de y (obtém o valor = 10) e incrementa 1
+	x++                                    // incrementa 1 em x
+	*y--                                   // desreferenciação de y (obtém o valor = 11) e decrementa 1
+	fmt.Println("Valor de y:", *y)         // 11
+	fmt.Println("Valor do ponteiro x:", x) // 11
 	// &: referenciação
 	// *: desreferenciação
+
+	arrayPtr()
+}
+
+func arrayPtr() {
+	var x [3]int = [3]int{1, 2, 3}
+	var y = &x[0] // y -> x[0] == 1
+	var z = &x[1] // z -> x[1] == 2
+	fmt.Printf("%d %d\n", *y, *z)
+	y = z                         // y -> z -> x[1] == 2
+	fmt.Printf("%d %d\n", *y, *z) // 2 2 (y e z apontam para o mesmo endereço de memória)
 }
