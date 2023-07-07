9054e73976d9d3ab6af56b16214a5925fde0d620
Maurício Witter <mauriciobw17@gmail.com>
2023-07-07 18:22:51 -0300

    implement functions

---
+++ 4-tests/intro/address/address.go
@@ -0,0 +1,7 @@
+package address
+
+func AddressType(address string) string {
+	addresses := []string{"Rua", "Avenida", "Estrada", "Rodovia"}
+
+	return addresses[0]
+}
---
+++ 4-tests/intro/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"address/address"
+	"fmt"
+)
+
+func main() {
+	address := address.AddressType("Rua 1")
+
+	fmt.Println(address)
+}
